bd700f1182ce4f9243d09eca477dd39ae0a7b2e2
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-08-20 16:01:45 -0300

    Implementação de testes arquivos asyncJest.spec.js

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,6 @@
-const assert = require('assert');
-// const answerPhone = require('../src/asyncJest');
+const answerPhone = require('../src/asyncJest');
+
+// jest.mock('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
@@ -12,11 +13,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('1 - O retorno do telefonema', () => {
   test('atende', () => {
-    
     // Insira seu teste assíncrono aqui
+    answerPhone(true).then((result) => {
+      expect(result).toBe('Oi!');
+    });
   });
   test('ocupado', () => {
-    
+    answerPhone(false).catch((error) => {
+      expect(error.message).toMatch('Infelizmente não podemos atender...');
+    });
     // Insira seu teste assíncrono aqui
   });
 });
